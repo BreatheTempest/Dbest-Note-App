f3a789e432417f98ec60be5de201a6308939467c
Anton Zabulonov <zabulonov.a@gmail.com>
2022-06-29 21:47:16 +0300

    Add styling to the sidebar

--- src/components/Notes/Sidebar.js
+++ src/components/Notes/Sidebar.js
@@ -1,24 +1,25 @@
 import React from 'react';
+import remove from '../../images/remove.svg';
 
 export default function Sidebar(props) {
-	console.log(props);
 	const noteElements = props.notes.map((note, index) => (
 		<div key={note.id}>
 			<div
-				className={`title ${
+				className={`note ${
 					note.id === props.currentNote.id ? 'selected-note' : ''
 				}`}
 				onClick={() => props.setCurrentNoteId(note.id)}
 			>
-				<h4 className="text-snippet">
-					{note.body.split('\n')[0] || `Empty note ${index + 1}`}
-				</h4>
-				<p>{note.date}</p>
+				<h2 className="note-title">Title</h2>
+				<p className="note-description">
+					{note.body.split('\n')[0].match(/[\w\s.,]/g, '') || `Empty note`}
+				</p>
+				<p className="note-date">{note.date}</p>
 				<button
 					className="delete-btn"
 					onClick={(e) => props.deleteNote(e, note.id)}
 				>
-					<i className="gg-trash trash-icon"></i>
+					<img src={remove} alt="" />
 				</button>
 			</div>
 		</div>
